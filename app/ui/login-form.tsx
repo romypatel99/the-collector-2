313884f8fb81300dc56fb57a2158df9757eceffe
Romil Patel <rpatel@stins.com>
2025-03-02 13:11:09 -0500

    fix login and redirect issue

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -15,7 +15,7 @@ import { useSearchParams } from 'next/navigation';
 export default function LoginForm() {
   
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard/invoices';
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined,
