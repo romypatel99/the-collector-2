11c404928071d9b1955450669c9467d03b1d4eba
Romil Patel <rpatel@stins.com>
2025-03-02 16:03:37 -0500

    add dynamic route

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,7 +3,8 @@ import CustomersTable from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
- 
+
+export const dynamic = 'force-dynamic';
 export default async function Page() {
     const [customers] = await Promise.all([fetchFilteredCustomers()]);
     console.log(customers)
