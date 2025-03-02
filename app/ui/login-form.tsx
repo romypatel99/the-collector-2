ac9467e29c4c13d0008e7c58598ed5b094caf685
Romil Patel <rpatel@stins.com>
2025-03-02 15:33:27 -0500

    revert to dashboard login

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -15,7 +15,7 @@ import { useSearchParams } from 'next/navigation';
 export default function LoginForm() {
   
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard/invoices';
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined,
