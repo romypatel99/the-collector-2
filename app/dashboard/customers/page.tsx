e124a997d4e10043d1e67f149cc1a4f04debe098
Romil Patel <rpatel@stins.com>
2025-03-02 15:56:25 -0500

    add skeleton for cust table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,6 +2,7 @@ import { lusitana } from '@/app/ui/fonts';
 import CustomersTable from '@/app/ui/customers/table';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
     const [customers] = await Promise.all([fetchFilteredCustomers()]);
@@ -9,7 +10,7 @@ export default async function Page() {
 
 return (
     <main>
-        <Suspense>
+        <Suspense fallback={<InvoicesTableSkeleton />}>
             <CustomersTable customers={customers} />
         </Suspense>
     </main>
